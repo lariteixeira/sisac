3f21fecd8ebc271ffdbe40cd83da9450f8794250
lariteixeira <larissateixeira@id.uff.br>
2015-08-05 12:03:04 -0300

    upload de mais de um arquivo, e mudanca no scopo do email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,9 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
   config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :letter_opener
+ config.action_mailer.delivery_method = :smtp
 config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
 
   # Adds additional error checking when serving assets at runtime.
